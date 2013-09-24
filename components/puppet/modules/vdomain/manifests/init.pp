10871d6eab91d93a33bf5fd6f9e0d0c88fd715a2
Paul Houghton <paul4hough@gmail.com>
2013-09-24 05:31:18 -0600

    WIP: Initial work to install and configure virtualization support
    refs #52, #49, #47

---
+++ components/puppet/modules/vdomain/manifests/init.pp
@@ -0,0 +1,8 @@
+# vdomain class - setup a virtual domain for product testing
+
+define vdomain (
+  $name = undef,
+  $os   ='fedora19'
+) {
+
+}
---
+++ components/puppet/modules/vdomain/spec/defines/vdomain_init_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'gitolite', :type => 'define' do
+
+end
