fc86c0a7cc39481863813099947b13f2f4a428d6
Paul Houghton <paul4hough@gmail.com>
2013-09-25 13:05:34 -0600

    Finally got all the dependencies for rspec-system-serverspec
    fixes #73

--- deps.pp
+++ deps.pp
@@ -5,109 +5,118 @@ node default {
 
   case $::osfamily {
     'Darwin': {
-    
-  	  package { 'python27' : 
-    	ensure   => 'installed',
-    	provider => 'macports'
-  	  }
-  	  package { 'py27-pip' :
-    	ensure   => 'installed',
-    	provider => 'macports',
-    	require  => Package['python27']
-  	  }
-  	  package { 'py-pip' :
-    	ensure   => 'installed',
-    	provider => 'macports',
-    	require  => Package['py27-pip']
-  	  }
-  	  	
-  	  package { 'python33' : 
-    	ensure   => 'installed',
-    	provider => 'macports'
-  	  }
-  	  package { 'py33-pip' :
-    	ensure   => 'installed',
-    	provider => 'macports',
-    	require  => Package['python33']
-  	  }
-  	  # ugg more pip provider issues - macports is /opt/local/bin/pip
-	  file { '/usr/bin/pip' :
-	    ensure     => 'link',
-	    target     => '/opt/local/bin/pip-2.7',
-	  }
-	  
-	  package { 'nose' : 
-	    ensure   => 'installed',
-	    provider => 'pip',
-	    require  => File['/usr/bin/pip'],
-	  }
-	   
-	  # :( no support for pip-3 yet
-	  exec { 'install_nose3' : 
-	    command  => '/opt/local/bin/pip-3.3 install nose',
-	    require  => Package['py33-pip'],
-	  }
-	  package { 'nose-cover3' : 
-	    ensure   => 'installed',
-	    provider => 'pip',
-	    require  => Package['nose'],
-	  }
-	   
-	  exec { 'install_nose3-cover3' : 
-	    command  => '/opt/local/bin/pip-3.3 install nose-cover3',
-	    require  => Package['py33-pip'],
-	  }
-	  package { 'nose-exclude' : 
-	    ensure   => 'installed',
-	    provider => 'pip',
-	    require  => Package['nose'],
-	  }
-	  exec { 'install_nose3-exclude' : 
-	    command  => '/opt/local/bin/pip-3.3 install nose-exclude',
-	    require  => Package['py33-pip'],
-	  }
-	}
-	
+
+      package { 'python27' :
+        ensure   => 'installed',
+        provider => 'macports'
+      }
+      package { 'py27-pip' :
+        ensure   => 'installed',
+        provider => 'macports',
+        require  => Package['python27']
+      }
+      package { 'py-pip' :
+        ensure   => 'installed',
+        provider => 'macports',
+        require  => Package['py27-pip']
+      }
+
+      package { 'python33' :
+        ensure   => 'installed',
+        provider => 'macports'
+      }
+      package { 'py33-pip' :
+        ensure   => 'installed',
+        provider => 'macports',
+        require  => Package['python33']
+      }
+      # ugg more pip provider issues - macports is /opt/local/bin/pip
+      file { '/usr/bin/pip' :
+        ensure     => 'link',
+        target     => '/opt/local/bin/pip-2.7',
+      }
+
+      package { 'nose' :
+        ensure   => 'installed',
+        provider => 'pip',
+        require  => File['/usr/bin/pip'],
+      }
+
+    # :( no support for pip-3 yet
+      exec { 'install_nose3' :
+        command  => '/opt/local/bin/pip-3.3 install nose',
+        require  => Package['py33-pip'],
+      }
+      package { 'nose-cover3' :
+        ensure   => 'installed',
+        provider => 'pip',
+        require  => Package['nose'],
+      }
+
+      exec { 'install_nose3-cover3' :
+        command  => '/opt/local/bin/pip-3.3 install nose-cover3',
+        require  => Package['py33-pip'],
+      }
+      package { 'nose-exclude' :
+        ensure   => 'installed',
+        provider => 'pip',
+        require  => Package['nose'],
+      }
+      exec { 'install_nose3-exclude' :
+        command  => '/opt/local/bin/pip-3.3 install nose-exclude',
+        require  => Package['py33-pip'],
+      }
+    }
+
     default: {
-	  package { 'python' : 
-	    ensure   => 'installed',
-	  }
-	  package { 'python-pip' :
-	    ensure   => 'installed',
-	  }
-  	  package { 'python3' : 
-    	ensure   => 'installed',
-  	  }  	  
-	  package { 'python3-pip' :
-	    ensure   => 'installed',
-	  }
-  	  
-	  package { 'python-nose' : 
-	    ensure   => 'installed',
-	    require  => Package['pthon'],
-	  }
-	  package { 'python-nose-cover3' : 
-	    ensure   => 'installed',
-	    require  => Package['pthon'],
-	  }
-	  package { 'python-nose-exclude' : 
-	    ensure   => 'installed',
-	    require  => Package['pthon'],
-	  }
-	  package { 'python3-nose' : 
-	    ensure   => 'installed',
-	    require  => Package['pthon3'],
-	  }
-	  package { 'python3-nose-cover3' : 
-	    ensure   => 'installed',
-	    require  => Package['pthon3'],
-	  }
-	  package { 'python3-nose-exclude' : 
-	    ensure   => 'installed',
-	    require  => Package['pthon3'],
-	  }
-	}
+
+      package { 'zfs-fuse' :
+        ensure    => 'installed',
+      }->
+      service { 'zfs-fuse' :
+        ensure    => 'running',
+        enable    => true,
+      }
+      package { 'python' :
+        ensure   => 'installed',
+      }
+      package { 'python-pip' :
+        ensure   => 'installed',
+      }
+      package { 'python3' :
+        ensure   => 'installed',
+      }
+      package { 'python3-pip' :
+        ensure   => 'installed',
+      }
+
+      package { 'python-nose' :
+        ensure   => 'installed',
+        require  => Package['python'],
+      }
+      package { 'python-nose-cover3' :
+        ensure   => 'installed',
+        require  => Package['python'],
+      }
+      package { 'python-nose-exclude' :
+        ensure   => 'installed',
+        require  => Package['python'],
+      }
+      package { 'python3-nose' :
+        ensure   => 'installed',
+        require  => Package['python3'],
+      }
+      package { 'python3-nose-cover3' :
+        ensure   => 'installed',
+        require  => Package['python3'],
+      }
+      package { 'python3-nose-exclude' :
+        ensure   => 'installed',
+        require  => Package['python3'],
+      }
+    }
   }
+
   package { 'rspec-puppet' :
     ensure   => 'installed',
     provider => 'gem',
@@ -119,5 +128,37 @@ node default {
   package { 'puppet-lint' :
     ensure   => 'installed',
     provider => 'gem',
+  }
+  package { 'puppet' :
+    ensure    => 'installed',
+    provider  => 'gem',
+  }
+  package { 'libxml2' :
+    ensure   => 'installed',
+  }->
+  package { 'libxml2-devel' :
+    ensure   => 'installed',
+  }->
+  package { 'libxslt' :
+    ensure   => 'installed',
+  }->
+  package { 'libxslt-devel' :
+    ensure   => 'installed',
+  }->
+  package { 'rspec-core' :
+    ensure   => '2.13.0',
+    provider => 'gem',
+  }->
+  package { 'rspec-mocks' :
+    ensure   => '2.13.0',
+    provider => 'gem',
+  }->
+  package { 'rspec-expectations' :
+    ensure   => '2.13.0',
+    provider => 'gem',
+  }->
+  package { 'rspec-system-serverspec' :
+    ensure    => 'installed',
+    provider  => 'gem',
   }
 }
