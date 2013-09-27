8435181924a0927f652292bc589eb946ba8350b1
Paul Houghton <paul4hough@gmail.com>
2013-09-27 01:46:49 -0600

    Opps left overs from conflict

--- deps.pp
+++ deps.pp
@@ -4,14 +4,9 @@
 node default {
 
   case $::osfamily {
-    'Darwin': {
-<<<<<<< HEAD
-    
-      package { 'python27' : 
-=======
 
+    'Darwin': {
       package { 'python27' :
->>>>>>> test-fail
         ensure   => 'installed',
         provider => 'macports'
       }
@@ -25,13 +20,7 @@ node default {
         provider => 'macports',
         require  => Package['py27-pip']
       }
-<<<<<<< HEAD
-        
-      package { 'python33' : 
-=======
-
       package { 'python33' :
->>>>>>> test-fail
         ensure   => 'installed',
         provider => 'macports'
       }
@@ -45,71 +34,35 @@ node default {
         ensure     => 'link',
         target     => '/opt/local/bin/pip-2.7',
       }
-<<<<<<< HEAD
-      
-      package { 'nose' : 
-=======
-
       package { 'nose' :
->>>>>>> test-fail
         ensure   => 'installed',
         provider => 'pip',
         require  => File['/usr/bin/pip'],
       }
-<<<<<<< HEAD
-       
       # :( no support for pip-3 yet
-      exec { 'install_nose3' : 
-        command  => '/opt/local/bin/pip-3.3 install nose',
-        require  => Package['py33-pip'],
-      }
-      package { 'nose-cover3' : 
-=======
-
-    # :( no support for pip-3 yet
       exec { 'install_nose3' :
         command  => '/opt/local/bin/pip-3.3 install nose',
         require  => Package['py33-pip'],
       }
       package { 'nose-cover3' :
->>>>>>> test-fail
         ensure   => 'installed',
         provider => 'pip',
-        require  => Package['nose'],
+        require  => File['/usr/bin/pip'],
       }
-<<<<<<< HEAD
-       
-      exec { 'install_nose3-cover3' : 
-        command  => '/opt/local/bin/pip-3.3 install nose-cover3',
-        require  => Package['py33-pip'],
-      }
-      package { 'nose-exclude' : 
-=======
-
       exec { 'install_nose3-cover3' :
         command  => '/opt/local/bin/pip-3.3 install nose-cover3',
         require  => Package['py33-pip'],
       }
       package { 'nose-exclude' :
->>>>>>> test-fail
         ensure   => 'installed',
         provider => 'pip',
         require  => Package['nose'],
       }
-<<<<<<< HEAD
-      exec { 'install_nose3-exclude' : 
-=======
       exec { 'install_nose3-exclude' :
->>>>>>> test-fail
         command  => '/opt/local/bin/pip-3.3 install nose-exclude',
         require  => Package['py33-pip'],
       }
     }
-<<<<<<< HEAD
-    
-    default: {
-      package { 'python' : 
-=======
 
     default: {
 
@@ -121,51 +74,17 @@ node default {
         enable    => true,
       }
       package { 'python' :
->>>>>>> test-fail
         ensure   => 'installed',
       }
       package { 'python-pip' :
         ensure   => 'installed',
       }
-<<<<<<< HEAD
-      package { 'python3' : 
-        ensure   => 'installed',
-      }       
-      package { 'python3-pip' :
-        ensure   => 'installed',
-      }
-      
-      package { 'python-nose' : 
-        ensure   => 'installed',
-        require  => Package['pthon'],
-      }
-      package { 'python-nose-cover3' : 
-        ensure   => 'installed',
-        require  => Package['pthon'],
-      }
-      package { 'python-nose-exclude' : 
-        ensure   => 'installed',
-        require  => Package['pthon'],
-      }
-      package { 'python3-nose' : 
-        ensure   => 'installed',
-        require  => Package['pthon3'],
-      }
-      package { 'python3-nose-cover3' : 
-        ensure   => 'installed',
-        require  => Package['pthon3'],
-      }
-      package { 'python3-nose-exclude' : 
-        ensure   => 'installed',
-        require  => Package['pthon3'],
-=======
       package { 'python3' :
         ensure   => 'installed',
       }
       package { 'python3-pip' :
         ensure   => 'installed',
       }
-
       package { 'python-nose' :
         ensure   => 'installed',
         require  => Package['python'],
@@ -189,7 +108,6 @@ node default {
       package { 'python3-nose-exclude' :
         ensure   => 'installed',
         require  => Package['python3'],
->>>>>>> test-fail
       }
     }
   }
